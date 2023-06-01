abc22c0428edf7915e81f9b59dc5bd7ab1e2d4c7
Jannik Brinkmann <jannik.brinkmann@uni-mannheim.de>
2023-06-01 09:52:47 +0200

    update dataclass_to_args

--- utils/args.py
+++ utils/args.py
@@ -3,7 +3,7 @@ import dataclasses
 
 
 def dataclass_to_args(dataclass_obj):
-    """converts a dataclass obj into an argparse obj"""
+    """converts a dataclass object into an argparse object"""
     parser = argparse.ArgumentParser()
     for field in dataclasses.fields(dataclass_obj):
         parser.add_argument(f"--{field.name}", default=getattr(dataclass_obj, field.name))
